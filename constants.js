d85c6725e9931807267b3826f19d5d32748b1b51
Keneth Lallemand <keneth.ortegal@perficient.com>
2023-07-13 00:19:04 -0500

    deleted previous index.js, preparing to be deployed as a module

--- constants.js
+++ constants.js
@@ -1,5 +1,6 @@
 export const TIMEOUT = "timeout"
 export const TIMEOUT_CODE = "fetch failed"
 export const TIMEOUT_LENGTH = 5000;
+export const TIMEOUT_GOOGLE_URL = "http://google.com:81/";
 export const IPIFY_URL = "https://api.ipify.org/"
 export const IPGEO_URL = "https://api.ipgeolocation.io/getip";
